f2cc3ce2b06bf770cb69874b2bd4eb1b98a91de8
turtle601 <turtle601@naver.com>
2024-05-27 14:53:12 +0900

    ✨ feat: 크롬 익스텐션에 Modal 컴포넌트 적용 [#2]

--- src/contentScript/ContentScript.tsx
+++ src/contentScript/ContentScript.tsx
@@ -1,5 +1,46 @@
+import Modal from '@/components/@common/Modal';
+import { useModalStore } from '@/store/modal';
+import { css } from '@emotion/react';
+import { useEffect } from 'react';
+import { useShallow } from 'zustand/react/shallow';
+
 function ContentScript() {
-  return <div>ContentScript</div>;
+  const { isOpen, open, close } = useModalStore(
+    useShallow((state) => ({
+      isOpen: state.isOpen,
+      open: state.openModal,
+      close: state.closeModal,
+    }))
+  );
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === 'b') {
+        e.preventDefault();
+        if (isOpen) {
+          close();
+        } else {
+          open(
+            <div
+              css={css({
+                color: 'white',
+              })}
+            >
+              안녕하세요
+            </div>
+          );
+        }
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isOpen]);
+
+  return <Modal element={document.getElementById('modal') as HTMLElement} />;
 }
 
 export default ContentScript;
--- src/contentScript/index.tsx
+++ src/contentScript/index.tsx
@@ -4,7 +4,11 @@ import ContentScript from '@/contentScript/ContentScript';
 
 function init() {
   const appContainer = document.createElement('div');
+  const modalContainer = document.createElement('div');
+  modalContainer.id = 'modal';
+
   document.body.appendChild(appContainer);
+  document.body.appendChild(modalContainer);
 
   const root = createRoot(appContainer);
   root.render(<ContentScript />);
