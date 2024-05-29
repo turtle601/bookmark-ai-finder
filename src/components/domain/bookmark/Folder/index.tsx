ed70c09293bc3bcca29d819856f2c701108362ed
turtle601 <turtle601@naver.com>
2024-05-29 14:33:58 +0900

    ✨ feat: 폴더 더블클릭시 세부 내용 보는 기능 구현 [#3]

--- src/components/domain/bookmark/Folder/index.tsx
+++ src/components/domain/bookmark/Folder/index.tsx
@@ -1,25 +1,31 @@
-import { color, spacer } from '@/styles/theme';
+import { useShallow } from 'zustand/react/shallow';
+
+import { color } from '@/styles/theme';
 
 import FolderIcon from '@/assets/folder.svg';
 
 import Flex from '@/components/@common/layout/Flex';
-import Spacer from '@/components/@common/layout/Spacer';
 import Text from '@/components/@common/Text';
+
+import { useBookmarkStore } from '@/store/bookmark';
 
 import type { FolderProps } from '@/components/domain/bookmark/Folder/type';
 
 function Folder({ title }: FolderProps) {
+  const { openFolder } = useBookmarkStore(
+    useShallow((state) => ({
+      openFolder: state.openFolder,
+    }))
+  );
+
   const viewFolderContent = () => {
-    console.log('폴더 내용 보기');
+    openFolder(title);
   };
 
   return (
-    <Flex>
-      <Flex direction="column" align="center" onDoubleClick={viewFolderContent}>
-        <FolderIcon />
-        <Spacer direction="vertical" space={spacer.spacing0} />
-        <Text color={color.white} label={title} />
-      </Flex>
+    <Flex direction="column" align="center" onDoubleClick={viewFolderContent}>
+      <FolderIcon />
+      <Text color={color.white} label={title} />
     </Flex>
   );
 }
