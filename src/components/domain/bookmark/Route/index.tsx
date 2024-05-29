b259292dbc30b27f3669396e887ea21255a150e7
turtle601 <turtle601@naver.com>
2024-05-29 15:53:20 +0900

    ✨ feat: 북마크 Route 컴포넌트 구현 [#3]
    Explained:
    - 북마크 폴더 경로를 나타내는 컴포넌트 구현
    - 기능: 폴더 경로 텍스트 클릭 시 해당 경로로 이동

---
+++ src/components/domain/bookmark/Route/index.tsx
@@ -0,0 +1,82 @@
+import { css } from '@emotion/react';
+import { MouseEventHandler, useMemo } from 'react';
+import { useShallow } from 'zustand/react/shallow';
+
+import Text from '@/components/@common/Text';
+import Flex from '@/components/@common/layout/Flex';
+import Spacer from '@/components/@common/layout/Spacer';
+
+import { color, spacer } from '@/styles/theme';
+import { useBookmarkStore } from '@/store/bookmark';
+
+import {
+  getFolderPath,
+  isLastListItem,
+  isRoot,
+} from '@/components/domain/bookmark/Route/util';
+
+import type { RouteProps } from '@/components/domain/bookmark/Route/type';
+
+function Route({ path }: RouteProps) {
+  const folderPathList = useMemo(getFolderPath(path), [path]);
+
+  const { pickFolder } = useBookmarkStore(
+    useShallow((state) => ({
+      pickFolder: state.pickFolder,
+    }))
+  );
+
+  const selectFolder =
+    (folder: string, idx: number): MouseEventHandler<HTMLButtonElement> =>
+    (e) => {
+      e.preventDefault();
+      pickFolder(isRoot(folder, idx) ? '' : folder);
+    };
+
+  return (
+    <Flex>
+      {folderPathList.map((folder, idx) => {
+        return (
+          <>
+            <button
+              css={css({
+                cursor: 'pointer',
+                backgroundColor: 'transparent',
+                outline: 'none',
+                border: 'none',
+              })}
+              type="button"
+              onClick={selectFolder(folder, idx)}
+            >
+              <Text
+                label={folder}
+                fontType="medium8"
+                fontWeightType="semibold"
+                color={color.white}
+                etcStyles={{
+                  '&:hover': {
+                    color: color.gray700,
+                  },
+                }}
+              />
+            </button>
+            {isLastListItem(folderPathList, idx) && (
+              <>
+                <Spacer direction="horizontal" space={spacer.spacing1} />
+                <Text
+                  label="-"
+                  fontType="medium8"
+                  fontWeightType="bold"
+                  color={color.white}
+                />
+                <Spacer direction="horizontal" space={spacer.spacing1} />
+              </>
+            )}
+          </>
+        );
+      })}
+    </Flex>
+  );
+}
+
+export default Route;
