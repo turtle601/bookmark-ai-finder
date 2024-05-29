76a13b64a0f32dba08e50433bf0432fbbd61bfee
turtle601 <turtle601@naver.com>
2024-05-29 16:01:46 +0900

    🔥 removed: 불필요한 콘솔 제거 [#3]

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,11 +1,3 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('I just installed my chrome extensions');
-});
-
-chrome.bookmarks.onCreated.addListener(() => {
-  console.log('I just bookmarked this page');
-});
-
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'getBookmarks') {
     chrome.bookmarks.getTree((tree) => {
