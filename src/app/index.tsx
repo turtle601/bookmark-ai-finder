775859aae0bcaf7b996ec582662353e7c67cb9ab
turtle601 <turtle601@naver.com>
2024-07-16 14:05:41 +0900

    ♻️ refactor: tanstack-query 설치 및 설정 [#15]

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,6 +4,8 @@ import Provider from '@/app/provider';
 
 export const makeContentScript = () => {
   const appContainer = document.createElement('div');
+  document.body.appendChild(appContainer);
+
   const root = createRoot(appContainer);
 
   root.render(<Provider />);
