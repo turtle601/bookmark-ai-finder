acfeeef568d67294806ff3b5267221412dfdaf72
turtle601 <turtle601@naver.com>
2024-07-19 15:52:33 +0900

    🔥 removed: screen 컴포넌트 관련 파일 제거 [#15]

--- src/shared/ui/screen/part/closer/index.ts
+++
@@ -1,3 +0,0 @@
-import Closer from '@/shared/ui/screen/part/closer/closer.ui';
-
-export default Closer;
--- src/shared/ui/screen/part/opener/index.ts
+++
@@ -1,3 +0,0 @@
-import Opener from '@/shared/ui/screen/part/opener/opener.ui';
-
-export default Opener;
