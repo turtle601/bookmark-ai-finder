70227eb5c7e3576ca5a2cd93ac48c615794e0c09
turtle601 <turtle601@naver.com>
2024-07-26 23:40:25 +0900

    ♻️ refactor: DnD 컴포넌트 리팩토링 [#19]

--- __stories__/shared/dnd.stories.tsx
+++ __stories__/shared/dnd.stories.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react';
+import React from 'react';
+
+import { useRef, useState } from 'react';
 
 import DnD from '@/shared/ui/dnd';
-import DnDProvider from '@/shared/ui/dnd/provider';
 
-import type { IDragPosition } from '@/shared/ui/dnd/hooks';
 import type { Meta, StoryObj } from '@storybook/react';
+import { css } from '@emotion/react';
+import { color } from '@/shared/config/styles';
+import Center from '@/shared/ui/center';
 
 const meta: Meta<typeof DnD> = {
   title: 'shared/DnD',
+
+  decorators: [
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    (Story) => <Story />,
+  ],
 };
 
 export default meta;
 
 type Story = StoryObj<typeof DnD>;
 
-interface IOnDropAction {
-  startPosition: IDragPosition;
-  endPosition: IDragPosition;
-}
-
 interface IItemType {
   id: string;
   text: string;
 }
 
-type ListType = IItemType[];
-
-const DnDComponent = () => {
-  const [list, setList] = useState<ListType>([
+const DnDComponent: React.FC = () => {
+  const dragItemId = useRef<number>(-1);
+  const [list, setList] = useState<IItemType[]>([
     {
       id: '0',
       text: 'Item 0',
@@ -36,19 +38,18 @@ const DnDComponent = () => {
       id: '1',
       text: 'Item 1',
     },
-
     {
       id: '2',
       text: 'Item 2',
     },
-
     {
       id: '3',
       text: 'Item 3',
     },
   ]);
 
-  const onDropMove = ({ startPosition, endPosition }: IOnDropAction) => {
+  const onDropMove = (id: number) => () => {
+    // 배열의 인덱스를 사용해라
     const newList = [
       ...list.flatMap((v) => {
         return [null, v];
@@ -56,8 +57,8 @@ const DnDComponent = () => {
       null,
     ];
 
-    const startId = startPosition.id * 2 + 1;
-    const endId = endPosition.id * 2;
+    const startId = dragItemId.current * 2 + 1;
+    const endId = id * 2;
 
     newList[endId] = newList[startId];
     newList[startId] = null;
@@ -65,27 +66,66 @@ const DnDComponent = () => {
     setList(newList.filter((v) => !!v));
   };
 
+  const onDragItem = (id: number) => () => {
+    dragItemId.current = id;
+  };
+
   return (
-    <DnDProvider>
-      <DnD.Overlay />
+    <ul>
       {list.map((item, id) => {
         return (
-          <div key={id}>
-            <DnD.DropableLine
-              action={onDropMove}
-              position={{ id: id, data: item }}
-            />
-            <DnD.DragableLine position={{ id: id, data: item }} />
-          </div>
+          <>
+            <DnD.Dropable dropAction={onDropMove(id)}>
+              {({ isDragEnter }) => {
+                return (
+                  <li
+                    css={css({
+                      width: '120px',
+                      height: '8px',
+                      backgroundColor: isDragEnter ? color.purple : color.white,
+                    })}
+                  ></li>
+                );
+              }}
+            </DnD.Dropable>
+            <DnD.Dragable key={item.id} dragAction={onDragItem(id)}>
+              {({ isDrag }) => {
+                return (
+                  <li
+                    css={css({
+                      width: '120px',
+                      height: '32px',
+                      backgroundColor: isDrag ? color.gray200 : color.white,
+                      padding: '8px',
+                    })}
+                  >
+                    {item.text}
+                  </li>
+                );
+              }}
+            </DnD.Dragable>
+          </>
         );
       })}
-      <DnD.DropableLine action={onDropMove} position={{ id: list.length }} />
-    </DnDProvider>
+    </ul>
   );
 };
 
 export const Default: Story = {
   render: () => {
-    return <DnDComponent />;
+    return (
+      <DnD.Provider>
+        <Center
+          direction={'column'}
+          etcStyles={{
+            width: '200px',
+            height: '300px',
+          }}
+        >
+          <DnD.PointerContent />
+          <DnDComponent />
+        </Center>
+      </DnD.Provider>
+    );
   },
 };
