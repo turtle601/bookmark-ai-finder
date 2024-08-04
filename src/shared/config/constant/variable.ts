0b61d885b4cf4798441c4d9ddf12273e4505c397
turtle601 <turtle601@naver.com>
2024-08-04 14:28:22 +0900

    ♻️ refactor: zIndex 레이어 수정 [#21]

--- src/shared/config/constant/variable.ts
+++ src/shared/config/constant/variable.ts
@@ -1,3 +1,3 @@
-export const FIRST_LAYER_ZIDENX = 10000000 as const;
-export const SECOND_LAYER_ZIDENX = 10000010 as const;
-export const THIRD_LAYER_ZIDENX = 10000100 as const;
+export const FIRST_LAYER_ZIDENX = 2147483646 as const;
+export const SECOND_LAYER_ZIDENX = 2147483746 as const;
+export const THIRD_LAYER_ZIDENX = 2147483846 as const;
