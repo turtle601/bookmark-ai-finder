36117ba97645878a051a69903e999ec481e92738
turtle601 <turtle601@naver.com>
2024-08-04 14:32:39 +0900

    ✨ feat: DnD.VerticalList 구현 [#21]
    DnD.VerticalList 구현 이유:
    DnD.boundary가 position:absolute 환경에서만 동작하기 때문
    따라서 List처럼 정렬하기 위해 각 요소의 높이를 걔산하여 렌더링

--- src/shared/ui/dnd/index.ts
+++ src/shared/ui/dnd/index.ts
@@ -2,12 +2,14 @@ import Provider from '@/shared/ui/dnd/part/provider';
 import Dragable from '@/shared/ui/dnd/part/dragable';
 import Dropable from '@/shared/ui/dnd/part/dropable';
 import Boundary from '@/shared/ui/dnd/part/boundary';
+import VerticalList from '@/shared/ui/dnd/part/list';
 import PointerContent from '@/shared/ui/dnd/part/pointerContent';
 
 import type { DragableFC } from '@/shared/ui/dnd/part/dragable/dragable.ui';
 import type { DropableFC } from '@/shared/ui/dnd/part/dropable/dropable.ui';
 import type { DnDProviderFC } from '@/shared/ui/dnd/part/provider/provider.ui';
 import type { BoundaryFC } from '@/shared/ui/dnd/part/boundary/boundary.ui';
+import type { VerticalListFC } from '@/shared/ui/dnd/part/list/list.ui';
 import type { PointerContentFC } from '@/shared/ui/dnd/part/pointerContent/pointerContent.ui';
 
 export interface IDnD {
@@ -15,6 +17,7 @@ export interface IDnD {
   Dropable: DropableFC;
   Provider: DnDProviderFC;
   Boundary: BoundaryFC;
+  VerticalList: VerticalListFC;
   PointerContent: PointerContentFC;
 }
 
@@ -25,6 +28,7 @@ const DnD: IDnD = Object.assign(
     Dropable,
     Provider,
     Boundary,
+    VerticalList,
     PointerContent,
   },
 );
---
+++ src/shared/ui/dnd/part/list/index.ts
@@ -0,0 +1,3 @@
+import VerticalList from '@/shared/ui/dnd/part/list/list.ui';
+
+export default VerticalList;
