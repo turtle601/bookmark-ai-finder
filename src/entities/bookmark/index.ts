86f37a0b5bf47241336f36fe2c047a5e8d7a562f
turtle601 <turtle601@naver.com>
2024-08-08 20:20:04 +0900

    🔥 removed: Bookmark 컴포넌트 제거 [#24]

--- src/entities/bookmark/index.ts
+++ src/entities/bookmark/index.ts
@@ -2,5 +2,3 @@ export * from './bookmark.api';
 export * from './bookmark.queries';
 export * from './bookmark.type';
 export * from './bookmark.mock';
-
-export { default as BookmarkNode } from './bookmark.ui';
