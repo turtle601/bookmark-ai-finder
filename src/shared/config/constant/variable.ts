e7c3e6ad6e8c08846c8a426b7c9e9087b24fe63a
turtle601 <turtle601@naver.com>
2024-08-09 16:07:39 +0900

    📦 chore: e.dataTransfer 관련 key 설정 [#24]

--- src/shared/config/constant/variable.ts
+++ src/shared/config/constant/variable.ts
@@ -1,3 +1,6 @@
 export const FIRST_LAYER_ZIDENX = 2147483646 as const;
 export const SECOND_LAYER_ZIDENX = 2147483746 as const;
 export const THIRD_LAYER_ZIDENX = 2147483846 as const;
+
+export const DND_BOOKMARK_KEY = 'BOOKMARK' as const;
+
