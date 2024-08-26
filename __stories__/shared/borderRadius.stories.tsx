019e5a9931c0174e0681fa84ebd963157a367750
turtle601 <turtle601@naver.com>
2024-08-26 19:31:03 +0900

    ♻️ refactor: borderRadius 스토리북 추가 [#32]

---
+++ __stories__/shared/borderRadius.stories.tsx
@@ -0,0 +1,121 @@
+import { css, CSSObject } from '@emotion/react';
+
+import { borderRadius, color } from '@/shared/config/styles';
+
+import type { StoryObj } from '@storybook/react';
+import Center from '@/shared/ui/center';
+
+const meta = {
+  title: 'shared/borderRadius',
+  tags: ['autodocs'],
+} as const;
+
+export default meta;
+
+type Story = StoryObj;
+
+const getGridItemStyle = (): CSSObject => {
+  return {
+    width: '80px',
+    height: '80px',
+  };
+};
+
+export const BorderRadiusTable: Story = {
+  render: () => {
+    return (
+      <div
+        css={css({
+          display: 'grid',
+          gridTemplateColumns: `repeat(3, 200px)`,
+          gap: '12px',
+        })}
+      >
+        <Center
+          etcStyles={{
+            ...getGridItemStyle(),
+          }}
+        >
+          small
+        </Center>
+        <Center
+          etcStyles={{
+            ...getGridItemStyle(),
+          }}
+        >
+          medium
+        </Center>
+        <Center
+          etcStyles={{
+            ...getGridItemStyle(),
+          }}
+        >
+          large
+        </Center>
+        <Center
+          etcStyles={{
+            ...getGridItemStyle(),
+          }}
+        >
+          12px
+        </Center>
+        <Center
+          etcStyles={{
+            ...getGridItemStyle(),
+          }}
+        >
+          20px
+        </Center>
+        <Center
+          etcStyles={{
+            ...getGridItemStyle(),
+          }}
+        >
+          32px
+        </Center>
+        <Center
+          etcStyles={{
+            ...getGridItemStyle(),
+          }}
+        >
+          <div
+            css={css({
+              width: '64px',
+              height: '64px',
+              backgroundColor: color.red300,
+              borderRadius: borderRadius.small,
+            })}
+          ></div>
+        </Center>
+        <Center
+          etcStyles={{
+            ...getGridItemStyle(),
+          }}
+        >
+          <div
+            css={css({
+              width: '64px',
+              height: '64px',
+              backgroundColor: color.red300,
+              borderRadius: borderRadius.medium,
+            })}
+          ></div>
+        </Center>
+        <Center
+          etcStyles={{
+            ...getGridItemStyle(),
+          }}
+        >
+          <div
+            css={css({
+              width: '64px',
+              height: '64px',
+              backgroundColor: color.red300,
+              borderRadius: borderRadius.large,
+            })}
+          ></div>
+        </Center>
+      </div>
+    );
+  },
+};
