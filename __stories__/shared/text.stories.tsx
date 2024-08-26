6045f7ab39bf6158eaeb88703814ea9d7bb125a8
turtle601 <turtle601@naver.com>
2024-08-26 19:19:20 +0900

    ♻️ refactor: Text 스토리북 리팩토링 [#32]

---
+++ __stories__/shared/text.stories.tsx
@@ -0,0 +1,61 @@
+import { spacer } from '@/shared/config/styles';
+import Spacer from '@/shared/ui/spacer';
+import Text from '@/shared/ui/text';
+import { css } from '@emotion/react';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Text> = {
+  title: 'shared/Text',
+  component: Text,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Text>;
+
+const message = 'The quick brown fox jumps over the lazy dog' as const;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <div
+        css={css({
+          display: 'grid',
+          gridTemplateColumns: `repeat(2, 1fr)`,
+          gap: spacer.spacing6,
+        })}
+      >
+        <div>
+          <Text label="lg" fontWeightType="bold" />
+          <Spacer direction="vertical" space={spacer['spacing2.5']} />
+          <Text label="size: 24px, font-family: Inter" />
+          <Spacer direction="vertical" space={spacer['spacing2.5']} />
+          <Text label={message} type="lg" />
+        </div>
+        <div>
+          <Text label="md" fontWeightType="bold" />
+          <Spacer direction="vertical" space={spacer['spacing2.5']} />
+          <Text label="size: 16px, font-family: Inter" />
+          <Spacer direction="vertical" space={spacer['spacing2.5']} />
+          <Text label={message} type="md" />
+        </div>
+        <div>
+          <Text label="sm" fontWeightType="bold" />
+          <Spacer direction="vertical" space={spacer['spacing2.5']} />
+          <Text label="size: 14px, font-family: Inter" />
+          <Spacer direction="vertical" space={spacer['spacing2.5']} />
+          <Text label={message} type="sm" />
+        </div>
+        <div>
+          <Text label="xs" fontWeightType="bold" />
+          <Spacer direction="vertical" space={spacer['spacing2.5']} />
+          <Text label="size: 12px, font-family: Inter" />
+          <Spacer direction="vertical" space={spacer['spacing2.5']} />
+          <Text label={message} type="xs" />
+        </div>
+      </div>
+    );
+  },
+};
