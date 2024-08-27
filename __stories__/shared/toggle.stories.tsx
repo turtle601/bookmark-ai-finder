4de374d9a4fbcb2e22b86e28ff42fe103e8f638a
turtle601 <turtle601@naver.com>
2024-08-27 15:09:08 +0900

    ♻️ refactor: toggle 스토리북 리팩토링 [#32]

--- __stories__/shared/toggle.stories.tsx
+++ __stories__/shared/toggle.stories.tsx
@@ -1,11 +1,21 @@
+import { spacer } from '@/shared/config/styles';
+
+import Text from '@/shared/ui/text';
+import Spacer from '@/shared/ui/spacer';
 import Toggle from '@/shared/ui/toggle';
+
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof Toggle> = {
   title: 'shared/Toggle',
   component: Toggle,
-  parameters: {
-    layout: 'centered',
+  argTypes: {
+    isChecked: {
+      description: '체크된 상태',
+    },
+    onClick: {
+      description: '토글 컴포넌트 클릭 시 발생하는 이벤트',
+    },
   },
 };
 
@@ -13,11 +23,18 @@ export default meta;
 
 type Story = StoryObj<typeof Toggle>;
 
-export const Default: Story = {
-  // args: {
-  //   externalAction: (isChecked) => {
-  //     const ment = isChecked ? '토글 켜기' : '토글 끄기';
-  //     console.log(ment);
-  //   },
-  // },
+export const ToggleTable: Story = {
+  render: () => {
+    return (
+      <div>
+        <Text label={'체크 상태'} fontWeightType="semibold" />
+        <Spacer direction="vertical" space={spacer.spacing2} />
+        <Toggle isChecked={false} onClick={() => {}} />
+        <Spacer direction="vertical" space={spacer.spacing4} />
+        <Text label={'체크되지 않은 상태'} fontWeightType="semibold" />
+        <Spacer direction="vertical" space={spacer.spacing2} />
+        <Toggle isChecked={true} onClick={() => {}} />
+      </div>
+    );
+  },
 };
