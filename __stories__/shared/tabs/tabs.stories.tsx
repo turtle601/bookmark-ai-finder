a97bb7ff95fde1e165b5721fd928d373d95b497d
turtle601 <turtle601@naver.com>
2024-08-27 12:45:05 +0900

    ♻️ refactor: tabs 스토리북 리팩토링 [#32]

--- __stories__/shared/tabs/tabs.stories.tsx
+++ __stories__/shared/tabs/tabs.stories.tsx
@@ -13,17 +13,19 @@ export default meta;
 
 type Story = StoryObj<typeof Tabs>;
 
-export const Default: Story = {
+export const Index: Story = {
   render: () => {
     return (
       <Tabs>
         <Tabs.TabList>
-          <Tabs.Tab>one</Tabs.Tab>
-          <Tabs.Tab>two</Tabs.Tab>
+          <Tabs.Tab>Tab 1</Tabs.Tab>
+          <Tabs.Tab>Tab 2</Tabs.Tab>
+          <Tabs.Tab>Tab 3</Tabs.Tab>
         </Tabs.TabList>
         <Tabs.TabPanels>
-          <Tabs.TabPanel>one</Tabs.TabPanel>
-          <Tabs.TabPanel>two</Tabs.TabPanel>
+          <Tabs.TabPanel>탭 내용 1</Tabs.TabPanel>
+          <Tabs.TabPanel>탭 내용 2</Tabs.TabPanel>
+          <Tabs.TabPanel>탭 내용 3</Tabs.TabPanel>
         </Tabs.TabPanels>
       </Tabs>
     );
