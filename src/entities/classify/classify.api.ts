2946a4160c0a875cdcb454b4cd6342dc4cc34599
turtle601 <turtle601@naver.com>
2024-09-01 13:45:18 +0900

    🐛 fix: AI 분류 타입 선택 UI에서 accordian Button 클릭 시 의도하지 않은 Accordian Panel이 열리는 버그 수정 [#39]

--- src/entities/classify/classify.api.ts
+++ src/entities/classify/classify.api.ts
@@ -16,7 +16,7 @@ export const classifyAIBookmarksMutation = async ({
   const bookmarkCache = bookmarkService.getCache();
   if (!bookmarkCache) return null;
 
-  const prompt = `${JSON.stringify(bookmarkCache)}에 있는 링크들을 ${JSON.stringify(categories)} 배열의 각 category 별로 링크들을 분류해줘. 이때 리턴 값은 { id, category: 카테고리명, bookmark: [{ id, title, url }] 형태의 배열값으로 줘. 그리고 category의 값이 중복되어서는 안돼. 부가적인 설명은 필요하지 않아.`;
+  const prompt = `${JSON.stringify(bookmarkCache)}에 있는 링크들을 ${JSON.stringify(categories)} 배열의 각 category 별로 링크들을 분류해줘. 이때 리턴 값은 { id, category: 카테고리명, bookmark: [{ id, title, url }] 형태의 배열값으로 줘. 그리고 category의 값이 중복되어서는 안돼. 그리고 id값은 '1','2','3'은 제외해줘. 부가적인 설명은 필요하지 않아.`;
   const response = await createGeminiRequest({ prompt });
 
   const result = mapAIBookmarks(response);
