8dfcb9bc88335d90f3d5b5a4ec12acf226c10c44
turtle601 <turtle601@naver.com>
2024-09-01 13:44:57 +0900

    🐛 fix: ai 카테고리 생성 시 카테고리가 중복으로 생성되는 버그 해결 [#35]

--- src/entities/category/category.api.ts
+++ src/entities/category/category.api.ts
@@ -10,7 +10,7 @@ export const categoryAIQuery = async (): Promise<ICategory[]> => {
   const bookmarkCache = bookmarkService.getCache();
   if (!bookmarkCache) return [];
 
-  const prompt = `${JSON.stringify(bookmarkCache)} 데이터를 기반, 어떠한 폴더를 기준으로 북마크를 분류해야할지 추천해줘. 리턴 값은 { id: 추천하는 폴더명, text: 추천하는 폴더명 }의 배열값으로 줘. 부가적인 설명은 필요하지 않아.`;
+  const prompt = `${JSON.stringify(bookmarkCache)} 데이터를 기반, 어떠한 폴더를 기준으로 북마크를 분류해야할지 추천해줘. 리턴 값은 { id: 추천하는 폴더명, text: 추천하는 폴더명 }의 배열값으로 줘. 이때, 중복된 폴더값과 북마크바와 기타 북마크, 모바일 북마크 폴더는 제외 시켜줘. 부가적인 설명은 필요하지 않아.`;
   const response = await createGeminiRequest({ prompt });
 
   const result = mapAICategorList(response);
